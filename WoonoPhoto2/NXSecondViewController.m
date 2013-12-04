95400d11bac07715d1472acc410df03a72726d48
woonohyo <wonhyo.yi@gmail.com>
2013-12-04 10:09:10 +0900

    DEP 1st

---
+++ WoonoPhoto2/NXSecondViewController.m
@@ -0,0 +1,38 @@
+//
+//  NXSecondViewController.m
+//  WoonoPhoto2
+//
+//  Created by Wonhyo Yi on 2013. 11. 27..
+//  Copyright (c) 2013년 NHN NEXT. All rights reserved.
+//
+
+#import "NXSecondViewController.h"
+
+@interface NXSecondViewController ()
+
+@end
+
+@implementation NXSecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
